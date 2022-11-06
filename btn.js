feab84125379d97fa13a29e65014de920206ff5a
KenDeclan <95083270+KenDeclan@users.noreply.github.com>
2022-11-06 22:08:27 +0300

    files

---
+++ btn.js
@@ -0,0 +1,10 @@
+var button= document.querySelector("button");
+var square =document.getElementById("btn");
+
+function changeBgColor(){
+    if(square.style.background =='blue'){
+        square.style.background='yellow';
+    }else{
+    square.style.background ="blue";
+}
+}
